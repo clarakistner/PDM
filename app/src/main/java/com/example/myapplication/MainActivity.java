9d016aab88b2c1f90a0f2890a6b45d5272f3481c
clara <clarabkistner@gmail.com>
2025-09-01 09:02:53 -0300

    app sorteio funcional

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,43 +12,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editTextMin, editTextMax;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Log.d("ciclo_vida", "OnCreate");
+        button = findViewById(R.id.button);
+        editTextMin = findViewById(R.id.edMin);
+        editTextMax = findViewById(R.id.edMax);
+        tv = findViewById(R.id.tvResultado);
+
+        button.setOnClickListener(v -> {
+            Random random = new Random();
+            int min, max;
+            min = Integer.parseInt(editTextMin.getText().toString());
+            max = Integer.parseInt(editTextMax.getText().toString());
+            int delta = max - min;
+            int sorteado = random.nextInt(delta)+min;
+            tv.setText(Integer.toString(sorteado));
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d( "ciclo_vida",  "OnStart");
-    }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d( "ciclo_vida",  "OnResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d( "ciclo_vida",  "OnPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d( "ciclo_vida",  "OnStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d( "ciclo_vida",  "OnDestroy");
-    }
 }

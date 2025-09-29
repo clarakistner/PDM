33b4370ec0c671ddb69b3c5120272c554f20d275
unknown <clarabkistner@gmail.com>
2025-09-29 09:08:12 -0300

    listView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,36 +17,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
-    EditText insira;
-    EditText editTextTextNumero;
+    String [] nomes = new String[] {"alguem", "pessoa", "ninguém", "individuo", "cidadao"};
+    ListView listView;
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button2);
-        editTextTextNumero = findViewById(R.id.editTextTextNumero);
-        insira = findViewById(R.id.Insira);
+        listView = findViewById(R.id.listView);
 
-        //textView.setText(Integer.toString(contador));
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, tabuadaPronta.class);
-            Bundle bundle = new Bundle();
-            int numeroi = Integer.parseInt((editTextTextNumero.getText().toString()));
-            bundle.putInt("numeroi", numeroi);
-            intent.putExtras((Intent) editTextTextNumero.getText());
-            startActivity(intent);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                nomes);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Toast.makeText(getApplicationContext(),nomes[position],Toast.LENGTH_LONG).show();
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
     }
 }

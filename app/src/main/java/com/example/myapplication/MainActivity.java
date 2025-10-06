73e9aad3686eb76fcdc862e0039d08466f19933f
clara <clarabkistner@gmail.com>
2025-10-06 08:23:48 -0300

    lista atualizada para apagar

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,10 +15,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    String [] nomes = new String[] {"alguem", "pessoa", "ninguém", "individuo", "cidadao"};
+    ArrayList<String> dados = new ArrayList<>();
+    Button button;
+    EditText edText;
     ListView listView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+        edText = findViewById(R.id.edText);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                android.R.id.text1,
-                nomes);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            Toast.makeText(getApplicationContext(),nomes[position],Toast.LENGTH_LONG).show();
+        button.setOnClickListener(v -> {
+            String texto = edText.getText().toString();
+            dados.add(texto);
+            edText.setText("");
+            adapter.notifyDataSetChanged();
         });
 
+        adapter = new ArrayAdapter<>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                dados);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Toast.makeText(getApplicationContext(), dados.get(position), Toast.LENGTH_LONG).show();
+        });
+
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+                dados.remove(dados.get(position));
+                adapter.notifyDataSetChanged();
+                return true;
+        });
 
     }
 }

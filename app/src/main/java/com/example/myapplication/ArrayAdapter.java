0600b9cd78d206260026e84a625a4d1ba8424857
clara <clarabkistner@gmail.com>
2025-11-26 10:59:47 -0300

    Laucher

--- app/src/main/java/com/example/myapplication/ArrayAdapter.java
+++ app/src/main/java/com/example/myapplication/ArrayAdapter.java
@@ -1,10 +1,11 @@
 package com.example.myapplication;
 
+import android.app.Application;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,10 +14,10 @@ import androidx.annotation.Nullable;
 
 import java.util.List;
 
-public class PlanetaAdapter extends ArrayAdapter<Planeta> {
+public class ArrayAdapter extends android.widget.ArrayAdapter<ApplicationInfo> {
 
     int nResource;
-    public PlanetaAdapter(@NonNull Context context, int resource, @NonNull List<Planeta> objects) {
+    public ArrayAdapter(@NonNull Context context, int resource, @NonNull List<ApplicationInfo> objects) {
         super(context, resource, objects);
         nResource = resource;
     }
@@ -26,11 +27,12 @@ public class PlanetaAdapter extends ArrayAdapter<Planeta> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         LayoutInflater layoutInflater = LayoutInflater.from(getContext());
         View v = layoutInflater.inflate(nResource, parent, false);
-        Planeta planeta = getItem(position);
-        TextView tv = v.findViewById(R.id.textView);
-        ImageView iv = v.findViewById(R.id.imageView);
-        tv.setText(planeta.nome);
-        iv.setImageResource(planeta.foto);
+        ApplicationInfo applicationInfo = getItem(position);
+        TextView name = v.findViewById(R.id.app_name);
+        ImageView icon = v.findViewById(R.id.app_icon);
+
+        name.setText(applicationInfo.loadLabel(getContext().getPackageManager()));
+        icon.setImageDrawable(applicationInfo.loadIcon(getContext().getPackageManager()));
         return v;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,27 +1,26 @@
 package com.example.myapplication;
 
-import android.annotation.SuppressLint;
+import android.app.Application;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
 
     ListView listView;
-    PlanetaController planetaController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +28,33 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
 
         setContentView(R.layout.activity_main);
-        listView = findViewById(R.id.listView);
-        planetaController = new PlanetaController();
+        ListView listView = findViewById(R.id.listView);
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PackageManager packageManager = getPackageManager();
+        List<ApplicationInfo> appList = packageManager.getInstalledApplications(PackageManager.GET_META_DATA);
 
-        PlanetaAdapter adapter = new PlanetaAdapter(
-                this, R.layout.item_lista,
-                planetaController.getPlaneta()
+        ArrayAdapter adapter = new ArrayAdapter(
+                this, R.layout.app_lista,
+                appList
         );
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ApplicationInfo applicationInfo = (ApplicationInfo) parent.getItemAtPosition(position);
+                String packageName = applicationInfo.packageName;
+                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
+
+                Intent intent = packageManager.getLaunchIntentForPackage(packageName);
+                if (intent != null) {
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Não foi possível abrir o aplicativo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
 
